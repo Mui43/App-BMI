c464ca60db7cabf5cb8a76226b1f4e1036ed0096
tanapat muijeen <tanapat.muijeen@gmail.com>
2024-06-26 14:04:20 +0700

    new

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,10 +6,36 @@ import { NavController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private nextpage: NavController) {}
+  weight ? : number;
+  hight ? : number;
+  result ? : number;
+  result_text : string = "";
 
-  gotologin(){
-    this.nextpage.navigateForward('/login');
+  constructor() {}
+
+  onCal(){
+    //console.log("w : " + this.weight)
+    //console.log("h : " + this.hight)
+    if(this.weight == undefined || this.hight == undefined){
+      alert("กรุณากรอกข้อมูลให้ครบ");
+      return;
+    }
+    var hi = this.hight / 100;
+    this.result = this.weight / (hi * hi)
+
+    //console.log(' ผลลัพธ์ ' + this.result)
+
+    if(this.result < 18.5){
+      this.result_text = "อยู่ในเกณฑ์น้ำหนักน้อยหรือผอม";
+    }else if(this.result >= 18.5 && this.result <= 22.99){
+      this.result_text = "อยู่ในเกณฑ์ปกติ";
+    }else if(this.result >= 23 && this.result <= 24.99){
+      this.result_text = "น้ำหนักเกิน";
+    }else if(this.result >= 25 && this.result <= 29.99){
+      this.result_text = "โรคอ้วนระดับ 1";
+    }else{
+      this.result_text = "โรคอ้วนระดับ 2";
+    }
   }
 
 }
